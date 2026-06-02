c85e1cabed482f38f4c312f5296d0ed40b12c53a
SUHO PARK <duglikespiano@gmail.com>
2026-06-03 01:02:16 +0900

    6 - LIST COMPREHENSIONS

--- 6-comprehensions/script.py
+++ 6-comprehensions/script.py
@@ -5,5 +5,17 @@
 # for x in range(10):
 #     values.append(x)
 
-values = [x for x in range(10)]
-print(values)
+# values = [x for x in range(10)]
+# print(values)
+
+
+# 2. Comprehension condition
+# Get all the even numbers from 0 - 50
+# evens = []
+# for number in range(50):
+#     is_even = number % 2 == 0
+#     if is_even:
+#         evens.append(number)
+
+evens = [number for number in range(50) if number % 2 == 0]
+print(evens)
